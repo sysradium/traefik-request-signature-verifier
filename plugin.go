5b3a076f7189c2298da647cff0e52ae71f823076
bast3 <bast33@yandex.ru>
2024-08-27 12:23:14 +0600

    use cache for keys

--- plugin.go
+++ plugin.go
@@ -3,8 +3,12 @@ package traefik_request_signature_verifier
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/kelseyhightower/envconfig"
 )
 
 type Config struct {
@@ -15,34 +19,47 @@ type Config struct {
 	Headers         []string
 	ResponseCode    int
 	DryRun          bool
+	RedisURI        string `envconfig:"REDIS_URI" required:"true"`
 }
 
 func CreateConfig() *Config {
-	return &Config{
+	config := &Config{
 		Headers:         make([]string, 0),
 		ResponseCode:    http.StatusForbidden,
 		ResponseMessage: "{\"error\": \"invalid checksum\"}",
 		SecretKey:       "62df864b-dd00-43e3-ac0a-5760ae26d3f5",
-		SignatureHeader: "X-Request-Signature",
 		DateHeader:      "X-Date",
 	}
+	if err := envconfig.Process("", config); err != nil {
+		log.Fatalf("Failed to process env config: %v", err)
+	}
+
+	return config
 }
 
 type signatureVerifier struct {
-	next     http.Handler
-	verifier *RequestVerifier
-	cfg      *Config
-	name     string
+	next        http.Handler
+	verifier    *RequestVerifier
+	cfg         *Config
+	name        string
+	redisClient *redis.Client
 }
 
 func New(_ context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	// Инициализация клиента Redis
+	opts, err := redis.ParseURL(config.RedisURI)
+	if err != nil {
+		return nil, err
+	}
+	redisClient := redis.NewClient(opts)
+	config.initSecretKey(redisClient)
 	return &signatureVerifier{
-		name: name,
-		next: next,
-		cfg:  config,
+		name:        name,
+		next:        next,
+		cfg:         config,
+		redisClient: redisClient,
 		verifier: &RequestVerifier{
 			headers:    config.Headers,
-			sigHeader:  config.SignatureHeader,
 			dateHeader: config.DateHeader,
 			secretKey: func() string {
 				return config.SecretKey
@@ -52,16 +69,60 @@ func New(_ context.Context, next http.Handler, config *Config, name string) (htt
 }
 
 func (r *signatureVerifier) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// Сначала пытаемся верифицировать запрос с использованием текущего ключа.
 	if err := r.verifier.VerifyRequest(req); err != nil {
 		if !errors.Is(err, ErrSignatureNotPresent) {
-			log.Printf("signature verification failed: %v", err)
+			log.Printf("signature verification failed with current key: %v", err)
 		}
 
-		if !r.cfg.DryRun {
-			http.Error(w, r.cfg.ResponseMessage, r.cfg.ResponseCode)
+		// Если верификация неудачна, пытаемся обновить ключ и повторить проверку.
+		if err := r.RetrieveAndUpdateKey(req); err != nil {
+			log.Printf("Error retrieving or updating key: %v", err)
+			http.Error(w, "Internal server error", http.StatusUnauthorized)
 			return
 		}
+
+	}
+	r.next.ServeHTTP(w, req)
+}
+
+func (cfg *Config) initSecretKey(redisClient *redis.Client) {
+	// Проверка, что URI для Redis предоставлен
+	if cfg.RedisURI == "" {
+		log.Println("REDIS_URI environment variable not set or empty")
+	}
+	ctx := context.Background()
+	key, err := redisClient.Get(ctx, "query-signature-key").Result()
+	if err != nil || key == "" {
+		log.Printf("Failed to retrieve initial key from Redis or key is empty: %v, using default key.", err)
+	} else {
+		cfg.SecretKey = key
+	}
+}
+
+func (r *signatureVerifier) RetrieveAndUpdateKey(req *http.Request) error {
+	ctx := context.Background()
+
+	// Попытка использования нового ключа из Redis
+	newKey, err := r.redisClient.Get(ctx, "query-signature-key").Result()
+	if err == nil {
+		if r.verifier.ValidateSignature(req, newKey) {
+			r.verifier.UpdateKey(newKey)
+			return nil
+		}
+	} else {
+		log.Printf("Error retrieving new key from Redis: %v", err)
 	}
 
-	r.next.ServeHTTP(w, req)
+	// Если новый ключ не прошел проверку, попробуем старый ключ
+	oldKey, err := r.redisClient.Get(ctx, "query-signature-key-old").Result()
+	if err == nil {
+		if r.verifier.ValidateSignature(req, oldKey) {
+			return nil
+		}
+	} else {
+		log.Printf("Error retrieving old key from Redis: %v", err)
+	}
+
+	return fmt.Errorf("no valid keys found")
 }
--- verifier.go
+++ verifier.go
@@ -20,12 +20,18 @@ var (
 
 type RequestVerifier struct {
 	secretKey  func() string
-	sigHeader  string
 	dateHeader string
 	headers    []string
 }
 
-func (v *RequestVerifier) Checksum(r *http.Request) string {
+func (v *RequestVerifier) Checksum(r *http.Request, key ...string) string {
+	// Используем переданный ключ, если он есть, иначе используем текущий ключ
+	var actualKey string
+	if len(key) > 0 {
+		actualKey = key[0]
+	} else {
+		actualKey = v.secretKey()
+	}
 	var (
 		data []byte
 		err  error
@@ -41,7 +47,7 @@ func (v *RequestVerifier) Checksum(r *http.Request) string {
 		}
 	}
 	hash := sha256.New()
-	hash.Write([]byte(v.secretKey()))
+	hash.Write([]byte(actualKey))
 
 	for _, h := range v.headers {
 		hash.Write([]byte(r.Header.Get(h)))
@@ -53,8 +59,8 @@ func (v *RequestVerifier) Checksum(r *http.Request) string {
 }
 
 func (v *RequestVerifier) VerifyRequest(r *http.Request) error {
-	sig := r.Header.Get(v.sigHeader)
-	if sig == "" {
+	sig, found := getSignatureHeader(r)
+	if !found {
 		return ErrSignatureNotPresent
 	}
 
@@ -95,4 +101,33 @@ func drainBody(b io.ReadCloser) (res io.ReadCloser, data []byte, err error) {
 		return io.NopCloser(&buf), nil, err
 	}
 	return io.NopCloser(&buf), buf.Bytes(), nil
+}
+
+// Helper function to validate the request signature with the given key
+func (v *RequestVerifier) ValidateSignature(r *http.Request, key ...string) bool {
+	sig, found := getSignatureHeader(r)
+	if !found {
+		return false
+	}
+	computedSignature := v.Checksum(r, key...)
+	return strings.ToLower(sig) == computedSignature
+}
+
+func (v *RequestVerifier) UpdateKey(newKey string) {
+	v.secretKey = func() string {
+		return newKey
+	}
+}
+
+func getSignatureHeader(r *http.Request) (string, bool) {
+	sig := r.Header.Get("X-Request-Signature")
+	if sig != "" {
+		return sig, true
+	}
+	sig = r.Header.Get("x-request-signature")
+	if sig != "" {
+		return sig, true
+	}
+
+	return "", false
 }
